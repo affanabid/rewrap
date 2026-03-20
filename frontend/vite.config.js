4006655565075f27af5837bd61952ac12bf68c3d
affan <affanabid31@gmail.com>
2026-03-20 15:07:11 +0500

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,17 +7,5 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 5173
-  },
-  build: {
-    target: 'esnext',
-    minify: 'terser',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'recharts': ['recharts']
-        }
-      }
-    }
   }
 })
